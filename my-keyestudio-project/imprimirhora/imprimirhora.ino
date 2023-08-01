411022c2561a66d7b369687e9bc167ae200ccbf4
miguepollo <127879645+miguepollo@users.noreply.github.com>
2023-08-01 08:47:54 +0200

    Add files via upload

---
+++ my-keyestudio-project/imprimirhora/imprimirhora.ino
@@ -0,0 +1,92 @@
+#include <WiFi.h>
+#include <Wire.h>
+#include <time.h>
+#include <LiquidCrystal_I2C.h>
+#include <ArduinoJson.h>        // https://github.com/bblanchon/ArduinoJson
+#include <HTTPClient.h>
+LiquidCrystal_I2C mylcd(0x27,16,2);
+long SleepDuration   = 10; // Sleep time in minutes, aligned to the nearest minute boundary.
+int  WakeupHour      = 7;  // Don't wakeup until after 07:00 to save battery power
+int  SleepHour       = 23; // Sleep after 23:00 to save battery power
+long StartTime       = 0;
+long SleepTimer      = 0;
+long Delta           = 30;
+const char* ssid = "MIWIFI_2G_y36Y";
+const char* password = "34sf36crhwnp";
+String  Time_str = "--:--:--";
+const char* Timezone    = "CET"; // Choose your time zone from: https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv 
+                                                           // See below for examples
+const char* ntpServer   = "0.es.pool.ntp.org"; // Or, choose a time server close to you, but in most cases it's best to use pool.ntp.org or time.google.com to find an NTP server
+                                                           // then the NTP system decides e.g. 0.pool.ntp.org, 1.pool.ntp.org as the NTP syem tries to find  the closest available servers
+                                                           // EU "0.europe.pool.ntp.org"
+                                                           // US "0.north-america.pool.ntp.org"
+                                                           // See: https://www.ntppool.org/en/                                                           
+int gmtOffset_sec     = +1;    // UK normal time is GMT, so GMT Offset is 0, for US (-5Hrs) is typically -18000, AU is typically (+8hrs) 28800
+int daylightOffset_sec = 3600; // In the UK DST is +1hr or 3600-secs, other countries may use 2hrs 7200 or 30-mins 1800 or 5.5hrs 19800 Ahead of GMT use + offset behind - offset
+
+boolean SetupTime() {
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer, "time.nist.gov"); //(gmtOffset_sec, daylightOffset_sec, ntpServer)
+  setenv("TZ", Timezone, 1);  //setenv()adds the "TZ" variable to the environment with a value TimeZone, only used if set to 1, 0 means no change
+  tzset(); // Set the TZ environment variable
+  delay(100);
+  return UpdateLocalTime();
+}
+
+
+void BeginSleep() {
+  mylcd.off();
+  UpdateLocalTime();
+  SleepTimer = (SleepDuration * 60 - ((CurrentMin % SleepDuration) * 60 + CurrentSec)) + Delta; //Some ESP32 have a RTC that is too fast to maintain accurate time, so add an offset
+  esp_sleep_enable_timer_wakeup(SleepTimer * 1000000LL); // in Secs, 1000000LL converts to Secs as unit = 1uSec
+  Serial.println("Awake for : " + String((millis() - StartTime) / 1000.0, 3) + "-secs");
+  Serial.println("Entering " + String(SleepTimer) + " (secs) of sleep time");
+  Serial.println("Starting deep-sleep period...");
+  esp_deep_sleep_start();  // Sleep for e.g. 30 minutes
+}
+
+boolean SetupTime() {
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer, "time.nist.gov"); //(gmtOffset_sec, daylightOffset_sec, ntpServer)
+  setenv("TZ", Timezone, 1);  //setenv()adds the "TZ" variable to the environment with a value TimeZone, only used if set to 1, 0 means no change
+  tzset(); // Set the TZ environment variable
+  delay(100);
+  return UpdateLocalTime();
+}
+
+void setup() {
+Serial.begin(115200);
+mylcd.init();
+mylcd.backlight();
+mylcd.clear();
+WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  if (StartWiFi() == WL_CONNECTED && SetupTime() == true) {
+    bool WakeUp = false;                
+    if (WakeupHour > SleepHour)
+      WakeUp = (CurrentHour >= WakeupHour || CurrentHour <= SleepHour); 
+    else                             
+      WakeUp = (CurrentHour >= WakeupHour && CurrentHour <= SleepHour);
+}
+void BeginSleep() {
+  epd_poweroff_all();
+  UpdateLocalTime();
+  SleepTimer = (SleepDuration * 60 - ((CurrentMin % SleepDuration) * 60 + CurrentSec)) + Delta; //Some ESP32 have a RTC that is too fast to maintain accurate time, so add an offset
+  esp_sleep_enable_timer_wakeup(SleepTimer * 1000000LL); // in Secs, 1000000LL converts to Secs as unit = 1uSec
+  Serial.println("Awake for : " + String((millis() - StartTime) / 1000.0, 3) + "-secs");
+  Serial.println("Entering " + String(SleepTimer) + " (secs) of sleep time");
+  Serial.println("Starting deep-sleep period...");
+  esp_deep_sleep_start();  // Sleep for e.g. 30 minutes
+}
+
+void loop() {
+SetupTime();
+Serial.println(Time_str);
+}
+
+
