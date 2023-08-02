86f1499cb97c921c523b8433dcd58230989dcb9d
miguepollo <miguelgarciapel@protonmail.com>
2023-08-02 12:10:20 +0200

    .

--- my-keyestudio-project/imprimirhora/imprimirhora.ino
+++ my-keyestudio-project/imprimirhora/imprimirhora.ino
@@ -11,6 +11,7 @@ int  SleepHour       = 23; // Sleep after 23:00 to save battery power
 long StartTime       = 0;
 long SleepTimer      = 0;
 long Delta           = 30;
+int     wifi_signal, CurrentHour = 0;
 const char* ssid = "MIWIFI_2G_y36Y";
 const char* password = "34sf36crhwnp";
 String  Time_str = "--:--:--";
@@ -23,6 +24,34 @@ const char* ntpServer   = "0.es.pool.ntp.org"; // Or, choose a time server close
                                                            // See: https://www.ntppool.org/en/                                                           
 int gmtOffset_sec     = +1;    // UK normal time is GMT, so GMT Offset is 0, for US (-5Hrs) is typically -18000, AU is typically (+8hrs) 28800
 int daylightOffset_sec = 3600; // In the UK DST is +1hr or 3600-secs, other countries may use 2hrs 7200 or 30-mins 1800 or 5.5hrs 19800 Ahead of GMT use + offset behind - offset
+
+boolean UpdateLocalTime() {
+  struct tm timeinfo;
+  char   time_output[30], day_output[30], update_time[30];
+  while (!getLocalTime(&timeinfo, 5000)) { // Wait for 5-sec for time to synchronise
+    Serial.println("Fallo al obtener tiempo");
+    return false;
+  }
+  CurrentHour = timeinfo.tm_hour;
+  CurrentMin  = timeinfo.tm_min;
+  CurrentSec  = timeinfo.tm_sec;
+  //See http://www.cplusplus.com/reference/ctime/strftime/
+  Serial.println(&timeinfo, "%a %b %d %Y   %H:%M:%S");      // Displays: Saturday, June 24 2017 14:05:49
+  if (Units == "M") {
+    sprintf(day_output, "%s, %02u %s %04u", weekday_D [timeinfo.tm_wday], timeinfo.tm_mday, month_M[timeinfo.tm_mon], (timeinfo.tm_year) + 1900);
+    strftime(update_time, sizeof(update_time), "%H:%M", &timeinfo);  // Creates: '@ 14:05:49'   and change from 30 to 8 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    sprintf(time_output, "%s", update_time);
+  }
+  else
+  {
+    strftime(day_output, sizeof(day_output), "%a %b-%d-%Y", &timeinfo); // Creates  'Sat May-31-2019'
+    strftime(update_time, sizeof(update_time), "%r", &timeinfo);        // Creates: '@ 02:05:49pm'
+    sprintf(time_output, "%s", update_time);
+  }
+  Date_str = day_output;
+  Time_str = time_output;
+  return true;
+}
 
 boolean SetupTime() {
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer, "time.nist.gov"); //(gmtOffset_sec, daylightOffset_sec, ntpServer)
