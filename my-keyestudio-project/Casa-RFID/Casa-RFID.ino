72437f15678fa251d078a378eb62e17f9c358113
miguepollo <miguelgarciapel@protonmail.com>
2023-09-13 18:08:00 +0200

    Remove void ShowReaderDetails

--- my-keyestudio-project/Casa-RFID/Casa-RFID.ino
+++ my-keyestudio-project/Casa-RFID/Casa-RFID.ino
@@ -99,21 +99,5 @@ void loop() {
   
 }
 
-void ShowReaderDetails() {
-  //  attain the MFRC522 software
-  byte v = mfrc522.PCD_ReadRegister(mfrc522.VersionReg);
-  Serial.print(F("MFRC522 Software Version: 0x"));
-  Serial.print(v, HEX);
-  if (v == 0x91)
-    Serial.print(F(" = v1.0"));
-  else if (v == 0x92)
-    Serial.print(F(" = v2.0"));
-  else
-    Serial.print(F(" (unknown)"));
-  Serial.println("");
-  // when returning to 0x00 or 0xFF, may fail to transmit communication signals
-  if ((v == 0x00) || (v == 0xFF)) {
-    Serial.println(F("WARNING: Communication failure, is the MFRC522 properly connected?"));
-  }
-}
+
 //**********************************************************************************
