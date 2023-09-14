6f5c8e09818d9c1021dbc67df9c6cbb952b246e3
miguepollo <miguelgarciapel@protonmail.com>
2023-09-14 16:55:24 +0200

    Add mylcd clear line

--- my-keyestudio-project/Casa-RFID/Casa-RFID.ino
+++ my-keyestudio-project/Casa-RFID/Casa-RFID.ino
@@ -80,6 +80,7 @@ void loop() {
   if (RFIDusadorecientemente == false)
   {
     if (xht.receive(dht)){
+      mylcd.clear(0 ,1)
       mylcd.setCursor(0, 1);
       mylcd.print(dht[2]);
       if (dht[2] >=28)
