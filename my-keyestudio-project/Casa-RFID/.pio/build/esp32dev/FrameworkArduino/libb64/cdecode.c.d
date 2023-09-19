5d41c03fb273c169767ac323c66b1000a9d7d432
miguepollo <miguelgarciapel@protonmail.com>
2023-09-19 17:36:10 +0200

    Add platformio things

---
+++ my-keyestudio-project/Casa-RFID/.pio/build/esp32dev/FrameworkArduino/libb64/cdecode.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/libb64/cdecode.c.o: \
+ /home/migue/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
+ /home/migue/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
