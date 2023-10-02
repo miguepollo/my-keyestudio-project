c0a5befdc305c28c3750b976e9f7d08cfa9f6c4c
miguepollo <miguelgarciapel@protonmail.com>
2023-10-02 19:21:12 +0200

    Remove platformio things

--- my-keyestudio-project/Casa-RFID/.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.o: \
- /home/migue/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- /home/migue/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- /home/migue/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- /home/migue/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- my-keyestudio-project/Casa-RFID/.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- /home/migue/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /home/migue/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
